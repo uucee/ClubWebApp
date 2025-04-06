ae252b03c4ee9bacd9cda92299ac582b68f9ed66
Uche Obiji <u_obiji@yahoo.com>
2025-04-06 19:53:23 +0100

    Update project structure and configuration

--- FC92_Club/FC92_Club/settings.py
+++ FC92_Club/FC92_Club/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'FC92_Club.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
